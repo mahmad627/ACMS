e69f5f2a15b3384a55712b7c9a1ee8dcfd4f48f4
ahmad <m.ahmad1ebcse292@gmail.com>
2020-09-08 14:24:07 +0500

    Done binding

--- app/src/main/java/com/example/acms/Login.java
+++ app/src/main/java/com/example/acms/Login.java
@@ -3,13 +3,22 @@ package com.example.acms;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+Button login;
+TextView signuptext;
+EditText email, password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         getSupportActionBar().hide();
+        login=findViewById(R.id.btn_login);
+        email=findViewById(R.id.edt_email);
+        password=findViewById(R.id.edt_password);
+        signuptext=findViewById(R.id.text_sidnup);
     }
 }
